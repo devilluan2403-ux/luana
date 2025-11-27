af032bc2857ab44335b5946199084f3d41af4117
devilluan2403-ux <devil.luan2403@gmail.com>
2025-11-27 11:33:47 +0700

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -6,28 +6,22 @@ const FILES_TO_CACHE = [
   '/apple-touch-icon.png',
   '/icon-192.png',
   '/icon-512.png',
-  // nếu bạn có CSS/JS riêng hãy thêm ở đây
 ];
 
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })
-    ))
-  );
+  evt.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })
+  )));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+    caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(() => caches.match('/index.html')))
   );
 });
-
