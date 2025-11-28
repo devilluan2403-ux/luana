408bcfa63e4fc0475e216cf79059bdb7008d6e8a
devilluan2403-ux <devil.luan2403@gmail.com>
2025-11-28 09:23:01 +0700

    Refactor service worker event listeners for brevity

--- sw.js
+++ sw.js
@@ -1,34 +1,25 @@
 const CACHE_NAME = 'ghi-can-cache-v1';
-
 const FILES_TO_CACHE = [
   './',
   './index.html',
+  './style.css',
+  './app.js',
   './manifest.json',
   './apple-touch-icon.png',
   './icon-192.png',
-  './icon-512.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', evt=>{
+  evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))
-    )
-  );
+self.addEventListener('activate', evt=>{
+  evt.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(resp =>
-      resp || fetch(evt.request).catch(() => caches.match('./index.html'))
-    )
-  );
+self.addEventListener('fetch', evt=>{
+  evt.respondWith(caches.match(evt.request).then(resp=>resp||fetch(evt.request).catch(()=>caches.match('./index.html'))));
 });
