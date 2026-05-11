642383f24dad0dc75cc5cd270edc2483790a7c55
devilluan2403-ux <devil.luan2403@gmail.com>
2026-05-11 20:26:30 +0700

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,20 +1,93 @@
-const CACHE = "sr-cache-v1";
+const CACHE = "sr-cache-v3";
+
 const FILES = [
-  "index.html",
-  "style.css",
-  "app.js"
-  // KHÔNG CACHE manifest hoặc icon
+
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
+
+/* =========================================
+                INSTALL
+========================================= */
+
+self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+
+    caches.open(CACHE)
+
+      .then(cache => {
+
+        return cache.addAll(FILES);
+      })
   );
 });
 
-// DÙNG NETWORK FIRST để icon, manifest luôn cập nhật
+
+/* =========================================
+                ACTIVATE
+========================================= */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE) {
+
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+
+/* =========================================
+                  FETCH
+========================================= */
+
 self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE)
+          .then(cache => {
+
+            cache.put(event.request, clone);
+          });
+
+        return response;
+      })
+
+      .catch(() => {
+
+        return caches.match(event.request)
+          .then(res => {
+
+            return res || caches.match("./index.html");
+          });
+      })
   );
 });
